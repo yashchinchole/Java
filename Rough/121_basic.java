2c70dcb5f91284a1a7228d7fe10c81457a458c21
YASH <iamyash337@gmail.com>
2023-01-21 20:25:07 +0530

    main

---
+++ Rough/121_basic.java
@@ -0,0 +1,7 @@
+package Github.Java.Rough;
+
+class Yash {
+    public static void main(String[] args) {
+        System.out.println("YASH CHINCHOLE");
+    }
+}
--- Rough/1_yash.java
+++
@@ -1,9 +0,0 @@
-package Github.Java.Rough;
-
-class yash {
-    public static void main(String[] args) {
-        System.out.println("YASH");
-        System.out.println(2 + 3);
-
-    }
-}
