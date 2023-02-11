0fb291faec0ded314900c5348e552e5947d92ac2
YASH <iamyash337@gmail.com>
2023-02-11 15:05:34 +0530

    main

---
+++ 1/basic.java
@@ -0,0 +1,7 @@
+public class basic {
+    public static void main(String[] args) {
+        System.out.println("YASH");
+        // System.out.println(args[0]);
+        // System.out.println(args[1]);
+    }
+}
