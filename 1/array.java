91afb4cd37fb7de09659c6047a858e3088b39304
YASH <iamyash337@gmail.com>
2023-02-16 23:58:21 +0530

    main

---
+++ 1/array.java
@@ -0,0 +1,11 @@
+public class array {
+    public static void main(String[] args) {
+        int a[] = new int[10];
+        a[0] = 5;
+        a[1] = 2;
+
+        for (int i = 0; i < 5; i++)
+            System.out.println(a[i]);
+
+    }
+}
