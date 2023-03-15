6ff4433462a4ed36cae2a427d8690907b240b32f
YASH <iamyash337@gmail.com>
2023-03-16 01:47:57 +0530

    main

---
+++ 2/inheritance.java
@@ -0,0 +1,27 @@
+class Father {
+    int age;
+}
+
+class Child extends Father {
+    void display() {
+        System.out.println(age);
+    }
+}
+
+class Daughter extends Father {
+}
+
+class GrandChild extends Child {
+    void display() {
+        System.out.println(age);
+    }
+}
+
+public class inheritance {
+    public static void main(String[] args) {
+        Child obj1 = new Child();
+        obj1.display();
+        GrandChild obj2 = new GrandChild();
+        obj2.display();
+    }
+}
